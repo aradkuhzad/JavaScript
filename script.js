5f035c5a32699da5df3b802406535925d64d4bf1
Arad <aradkuhzad@gmail.com>
2026-02-14 19:19:35 +0330

    Refactor script.js to improve code formatting and readability by adjusting indentation for consistency across functions.

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 function ShowDate() {
-    document.getElementById("demo").innerHTML = Date();
+  document.getElementById("demo").innerHTML = Date();
 }
 function Show() {
-    document.getElementById("showmessage").innerHTML = "Hello World";
+  document.getElementById("showmessage").innerHTML = "Hello World";
 }
 function activate() {
-    document.getElementById("picture-lamp").src = "img/pic_bulbon.gif";
+  document.getElementById("picture-lamp").src = "img/pic_bulbon.gif";
 }
 function deactivate() {
-    document.getElementById("picture-lamp").src = "img/pic_bulboff.gif";
+  document.getElementById("picture-lamp").src = "img/pic_bulboff.gif";
 }
 function changefont() {
-    document.getElementById("change-fontsize").style.fontSize = "40px";
+  document.getElementById("change-fontsize").style.fontSize = "40px";
 }
 function hidetext() {
-    document.getElementById("hide-text").style.display = "none";
+  document.getElementById("hide-text").style.display = "none";
 }
 function showtext() {
-    document.getElementById("show-text").style.display = "block";
+  document.getElementById("show-text").style.display = "block";
 }
 // window.alert ("Hello There!");  
 {
@@ -28,9 +28,10 @@ console.log({a:1, b:2});
 }
 
 function sorttext() {
-    let x = 1;
-    const fname = "David";
-    document.getElementsByClassName("text-yn")[0].innerHTML = fname + " is " + x + " years " + " old ";
+  let x = 1;
+  const fname = "David";
+  document.getElementsByClassName("text-yn")[0].innerHTML = 
+  fname + " is " + x + " years " + " old ";
 }
 sorttext();
 
