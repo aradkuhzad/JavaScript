54cba047edf7c2f662305101745c5b34ee0e2cae
Arad <aradkuhzad@gmail.com>
2026-02-14 17:14:00 +0330

    Add functions in script.js for lamp activation, font size adjustment, and text visibility control; include an alert greeting for user engagement.

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ function ShowDate() {
 function Show() {
     document.getElementById("showmessage").innerHTML = "Hello World";
 }
+function activate() {
+    document.getElementById("picture-lamp").src = "img/pic_bulbon.gif";
+}
+function deactivate() {
+    document.getElementById("picture-lamp").src = "img/pic_bulboff.gif";
+}
 
